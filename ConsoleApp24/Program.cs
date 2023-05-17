a869c1acf8be001df2e2234d54b58460076e24ae
NHOY <NHOY@2210DR000919>
2023-05-17 11:14:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int crystals;
+            int crystalPrice = 10;
+
+            Console.WriteLine("Эй, путник! У меня есть кристаллы для тебя. Один кристал всего по " + crystalPrice + " монет.");
+            Console.Write("Сколько у вас золота: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько вам нужно кристаллов: ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+
+            money -= crystals * crystalPrice;
+
+            Console.WriteLine("У вас в сумке - " + crystals + " кристаллов и " + money + " монет.");
+        }
+    }
+}
